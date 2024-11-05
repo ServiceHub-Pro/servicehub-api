6feedee0a06a3eb69ba90edc842cf2b69a80bc33
abubaimoro <abubakarimoro27@gmail.com>
2024-11-05 21:43:38 +0000

    significant updates to major folders and files

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-// import serviceRouter from "./routes/services-routes.js";
-// import providerRouter from "./routes/provider-routes.js";
-// import cors from "cors";
+import serviceRouter from "./routes/services-routes.js";
+import providerRouter from "./routes/provider-routes.js";
+import cors from "cors";
 
 // set up port
 const PORT = 4500;
@@ -14,12 +14,12 @@ const app = express();
 await mongoose.connect(process.env.MONGO_URI);
 
 // use middlewares
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
 // use routes
-// app.use(serviceRouter);
-// app.use(providerRouter);
+app.use(serviceRouter);
+app.use(providerRouter);
 
 // listen for incoming requests
 app.listen(PORT, () => {
