b73487c43373e49535b7756472cd23187a67ba88
abubaimoro <abubakarimoro27@gmail.com>
2024-11-17 15:13:11 +0000

    login user role added

--- validators/user-validators.js
+++ validators/user-validators.js
@@ -11,7 +11,8 @@ export const registerUserValidator = Joi.object({
 
 export const loginUserValidator = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().required()
+    password: Joi.string().required(),
+    role: Joi.string().valid('user','provider')
 });
 
 export const updateProfileValidator = Joi.object({
